3572a1807304924229a49581bbb98686fe7329be
Ioana Moflic <30651672+ioanamoflic@users.noreply.github.com>
2022-11-23 14:15:35 +0200

    Added success rate of expedition based on different params

--- peaks.py
+++ peaks.py
@@ -12,52 +12,66 @@ import numpy as np
 
 df = pd.read_csv('expeditions.csv')
 
+
 def get_timespan(datetime, timespan_name):
     if timespan_name  == 'month':
         return datetime.month
     return datetime.year
 
 
-    for i, expedition in df.iterrows():
-        timespan = get_timespan(pd.to_datetime(expedition['basecamp_date']), timespan_name)
-        if timespan not in all_results:
-            all_results[timespan] = 1
-        else:
-            all_results[timespan] += 1
-
-def get_success_percentage(timespan_name):
+def get_success_percentage(feature: str, df):
     succ_results = {}
     all_results = {}
     per_results = {}
 
     for i, expedition in df.iterrows():
-        timespan = get_timespan(pd.to_datetime(expedition['basecamp_date']), timespan_name)
-        if timespan not in all_results:
-            all_results[timespan] = 1
+        if feature == 'month' or feature == 'year':
+            feature_value = get_timespan(pd.to_datetime(expedition['basecamp_date']), feature)
         else:
-            all_results[timespan] += 1
+            feature_value = expedition[feature]
+
+        if feature_value not in all_results:
+            all_results[feature_value] = 1
+        else:
+            all_results[feature_value] += 1
 
         if expedition['termination_reason'].split()[0] == 'Success':
-            if timespan not in succ_results:
-                succ_results[timespan] = 1
+            if feature_value not in succ_results:
+                succ_results[feature_value] = 1
             else:
-                succ_results[timespan] += 1
+                succ_results[feature_value] += 1
 
     for key in all_results:
-        if isinstance(key, int) and key in succ_results:
+        if (isinstance(key, int) or isinstance(key, bool)) and key in succ_results:
             per_results[key] = succ_results[key] / all_results[key]
-        elif isinstance(key, int):
+        elif isinstance(key, int) or isinstance(key, bool):
             per_results[key] = 0
+
+    #print(dict(sorted(all_results.items(), key=lambda x: x[0])))
     return dict(sorted(per_results.items(), key=lambda x: x[0]))
 
 
-per_results = get_success_percentage('month')
+def save_plot(feature, df):
+    per_results = get_success_percentage(feature, df)
+    plt.plot(per_results.keys(), per_results.values())
+    plt.savefig(f'success_per_{feature}.png', dpi=300)
+    plt.clf()
 
-plt.plot(per_results.keys(), per_results.values())
-plt.savefig(f'success_per_month.png', dpi=300)
-plt.clf()
+
+print(df['peak_name'].value_counts())
+peaks = ['Everest', 'Ama Dablam', 'Cho Oyu']
+df_peaks = df[df["peak_name"].isin(peaks)]
+df_peaks_subset = df_peaks[df_peaks['hired_staff'] < 20]
+
+df_peaks_subset['year'] = pd.to_datetime(df_peaks['basecamp_date'])
+
+print(df_peaks_subset.head())
+
+df_peaks_subset = df_peaks_subset[pd.to_datetime(df_peaks_subset['basecamp_date']).year > 1981]
+
+save_plot('month', df_peaks_subset)
+save_plot('year', df_peaks_subset)
+save_plot('oxygen_used', df_peaks_subset)
+save_plot('hired_staff', df_peaks_subset)
 
 
-per_results = get_success_percentage('year')
-plt.plot(per_results.keys(), per_results.values())
-plt.savefig(f'success_per_year.png', dpi=300)
